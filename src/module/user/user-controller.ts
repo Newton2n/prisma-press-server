241bff4600797f27fd5c474c195198326758bf4c
Newton <156936866+Newton2n@users.noreply.github.com>
2026-06-25 00:15:32 +0600

    fix(users) : modularize users route

--- src/module/user/user-controller.ts
+++ src/module/user/user-controller.ts
@@ -1,56 +1,28 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { prisma } from "../../lib/prisma";
-import bcrypt from "bcryptjs";
-import config from "../../config";
+import { userService } from "./user-service";
+
 //register user
 export const registerUser = async (req: Request, res: Response) => {
-  const { name, email, password, profilePhotoUrl, bio } = req.body;
+  const payload = req.body;
 
-  //   const isUserRegistered = await prisma.user.findUnique({
-  //     where: {
-  //       email: email,
-  //     },
-  //   });
+  try {
+    const result = await userService.registerUserIntoDb(payload);
 
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-
-  const createdUser = await prisma.user.create({
-    data: {
-      name: name,
-      email: email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      profilePhoto: profilePhotoUrl,
-      bio: bio,
-      userId: createdUser.id,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: createdUser.email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-
-  res.status(StatusCodes.CREATED).json({
-    success: true,
-    message: "user created successful",
-    data: {
-      user: user,
-    },
-  });
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "user created successful",
+      data: {
+        user: result,
+      },
+    });
+  } catch (error: unknown) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Something went wrong ",
+      StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      errorDetails: (error as Error).message,
+    });
+  }
 };
---
+++ src/module/user/user-service.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import config from "../../config";
+import { RegisterUserPayload } from "./user-interface";
+
+const registerUserIntoDb = async (payload: RegisterUserPayload) => {
+  const { name, email, password, profilePhotoUrl, bio } = payload;
+  const isUserRegistered = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (isUserRegistered) {
+    throw new Error("The email already registered");
+  }
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  await prisma.profile.create({
+    data: {
+      profilePhoto: profilePhotoUrl,
+      bio: bio,
+      userId: createdUser.id,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: createdUser.email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  return user;
+};
+
+export const userService = { registerUserIntoDb };
