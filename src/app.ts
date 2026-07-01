4dce6f59c27b545e5898ee35333adb4b3baf29b0
Newton <156936866+Newton2n@users.noreply.github.com>
2026-07-01 16:58:29 +0600

    feat(not-found) :add not found page

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application} from "express";
 import cors from "cors";
 import config from "./config";
 import authRouter from "./module/auth/auth.route";
 import userRouter from "./module/user/user.route";
 import postRouter from "./module/post/post.route";
 import commentRouter from "./module/comment/comment.route";
+import notFound from "./middleware/notFound";
 const app: Application = express();
 
 app.use(
@@ -22,5 +23,8 @@ app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
+
+// error handle
+app.use(notFound);
 
 export default app;
---
+++ src/middleware/notFound.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    status: 404,
+    error: "Not Found",
+    requestType : req.method ,
+    message: `The requested URL ${req.originalUrl} was not found on this server.`,
+    time :new Date()
+  });
+};
