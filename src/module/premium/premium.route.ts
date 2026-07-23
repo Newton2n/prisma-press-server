f5f5b7304c7719ae3119ea1c5071c0dee065e5f2
Newton <156936866+Newton2n@users.noreply.github.com>
2026-07-23 23:07:54 +0600

    chore(subscription) : add subscription guard in get premium post endpoint

--- src/module/premium/premium.route.ts
+++ src/module/premium/premium.route.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
 import { premiumController } from "./premium.controller";
 import { authMiddleware } from "../../middleware/auth";
+import { subscriptionGuard } from "../../middleware/premium-guard";
 
 const router = Router();
 
 router.get(
   "/",
 authMiddleware.auth(Role.ADMIN, Role.AUTHOR, Role.USER),
-//   subscriptionGuard(),
+  subscriptionGuard(),
   premiumController.getPremiumContent,
 );
 
